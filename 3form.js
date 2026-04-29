cba5a1443a67b05e59552bac92e65f454be20455
justinlai2325 <justinlai@sekolahbim.sch.id>
2026-04-29 11:04:56 +0700

    Add files via upload

---
+++ 3form.js
@@ -0,0 +1,9 @@
+<form onsubmit="redirectPage(); return false;">
+  <button type="submit">Submit</button>
+</form>
+
+<script>
+function redirectPage() {
+  window.location.href = "file:///C:/Users/ITLABSMA/Documents/Justin%20Musk/project/3menu.html";
+}
+</script>
